173d77306e99a77bb149bae50e35f2b76bfd9beb
Helena Freire <helenamcfreire@gmail.com>
2013-10-05 16:17:53 -0300

    landing parte 5

---
+++ app/assets/javascripts/loompa.js
@@ -0,0 +1,9 @@
+!function ($) {
+
+	$(function(){
+		// carousel demo
+		$('#myCarousel').carousel({});
+	})
+
+
+}(window.jQuery)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoompaLanding::Application.config.secret_token = '097c4d7912b43d67d40b2477b547c8badffff3ac33320263f4ec9d97f990f59559f81b708a16266bdef556d54482c98a3a21d6a7af5d4f6cc7d07158470b0e86'
